ba432208235fdde6a0f26e39806cd6a17e46a5b7
raymond <ray710mond@gmail.com>
2026-03-10 17:00:09 +0000

    fixed bs

--- ros2_ws/src/Drivers/Sensors/IMU/imu_driver/launch/imu_driver.launch.py
+++ ros2_ws/src/Drivers/Sensors/IMU/imu_driver/launch/imu_driver.launch.py
@@ -1,7 +1,8 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
 
 
 def generate_launch_description():
@@ -12,14 +13,15 @@ def generate_launch_description():
 	)
 
 	namespace = LaunchConfiguration('namespace')
-	if namespace == 'bigred1':
-		return LaunchDescription([
-			namespace_arg,
-			Node(
-				package='imu_driver',
-				executable='imu_driver',
-				name='imu_driver',
-				namespace=namespace,
-				output='screen',
-			),
-		])
+
+	return LaunchDescription([
+		namespace_arg,
+		Node(
+			package='imu_driver',
+			executable='imu_driver',
+			name='imu_driver',
+			namespace=namespace,
+			output='screen',
+			condition=IfCondition(PythonExpression(["'", namespace, "' == 'bigred1'"])),
+		),
+	])
